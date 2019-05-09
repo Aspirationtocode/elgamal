e3c9a0023754702c97ff18f25a3b1007989e2209
Aspirationtocode <aspiretocode@gmail.com>
2019-05-09 13:33:01 +0300

    build

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { Crypto } from "./Crypto";
-
-const message = "Я люблю Катюшу!";
-
-// Get key pair
-const keyPair = Crypto.generateKeyPair(256);
-
-// Encrypt message public key
-const encryptedMessage = Crypto.encrypt(message, keyPair.pub);
-
-// Decrypt message with secret key
-const decryptedMessage = Crypto.decrypt(encryptedMessage, keyPair.sec);
-console.log(decryptedMessage);
+export { Crypto } from "./Crypto";
